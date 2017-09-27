16e25a077e4d2bcd889309c4cc4e50243eca1323
maxl <mrliucan@foxmail.com>
2017-09-27 12:46:24 +1000

    Cleaer test.js

--- controllers/test.js
+++ controllers/test.js
@@ -27,53 +27,8 @@ exports.getTest = (req, res) => {
 
   log.debug('DLKJLFJ');
 
-  const dbConfig = {
-    server: 'MAX2',
-    user: 'admin',
-    password: 'admin'
-  };
-
-  function getEmp() {
-    const conn = new sql.ConnectionPool(dbConfig);
-    const req = new sql.Request(conn);
-
-    conn.connect((err) => {
-      if (err) {
-        log.error(err);
-        return;
-      }
-      // req.query(`
-      // BACKUP DATABASE Crown_CMS TO DISK = 'C:\\Temp\\Crown_CMS.BAK'
-      // `, (err, recordset) => {
-      //   if (err) {
-      //     log.error(err);
-      //   } else {
-      //     log.debug(recordset);
-      //     console.log(recordset);
-      //   }
-      //   conn.close();
-      // });
-
-      req.query(`
-      USE [master]
-      RESTORE DATABASE [TEST_CMS] FROM  DISK = N'C:\\temp\\Crown_CMS.BAK' WITH  FILE = 1,  MOVE N'Crown_CMS_Data' TO N'C:\\Program Files\\Microsoft SQL Server\\MSSQL13.MSSQLSERVER\\MSSQL\\DATA\\TEST_CMS.mdf',  MOVE N'Crown_CMS_Log' TO N'C:\\Program Files\\Microsoft SQL Server\\MSSQL13.MSSQLSERVER\\MSSQL\\DATA\\TEST_CMS.ldf',  NOUNLOAD,  STATS = 5      
-      `, (err, recordset) => {
-        if (err) {
-          log.error(err);
-        } else {
-          log.debug(recordset);
-          console.log(recordset);
-        }
-        conn.close();
-      });
-    });
-  }
-
-  getEmp();
-
   res.render('test', {
-    // title: 'NewWeb',
-    // ngWeb
+    title: 'test',
   });
 };
 

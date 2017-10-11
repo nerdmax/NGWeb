096ef721522683b4b83b1924a4be4b574f8da361
maxl <mrliucan@foxmail.com>
2017-10-11 15:50:44 +1100

    1. Fix github.pug's repo link issue 2. Add NGWeb to github.pug

--- controllers/api.js
+++ controllers/api.js
@@ -119,11 +119,11 @@ exports.getScraping = (req, res, next) => {
  */
 exports.getGithub = (req, res, next) => {
   const github = new GitHub();
-  github.repos.get({ owner: 'sahat', repo: 'hackathon-starter' }, (err, repo) => {
+  github.repos.get({ owner: 'nerdmax', repo: 'NGWeb' }, (err, repo) => {
     if (err) { return next(err); }
     res.render('api/github', {
       title: 'GitHub API',
-      repo
+      repo: repo.data,
     });
   });
 };
